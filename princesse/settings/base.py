49867b542eb2d95f80362fb8e6301d6107623e99
Nicolas Mendez <nicolas.mendez@t-mobs.com>
2025-01-06 14:30:21 -0300

    collect static

--- princesse/settings/base.py
+++ princesse/settings/base.py
@@ -118,6 +118,9 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
+STATIC_ROOT = root_join('..', 'var', 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIR = (os.path.join(BASE_DIR, 'static'),)
 
