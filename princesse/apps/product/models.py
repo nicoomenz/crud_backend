9f62074e34037902bbeb862ae02be5d3a0fc89d3
Nicolas Mendez <nicolas.mendez@t-mobs.com>
2025-04-24 12:59:39 -0300

    devuelvo los productos si borro un payment desde el borrador

--- princesse/apps/product/models.py
+++ princesse/apps/product/models.py
@@ -179,11 +179,19 @@ class Producto(models.Model):
         ]
 
 class CustomProduct(models.Model):
+    payment = models.ForeignKey(
+        'payment.Payment',  # Apunta al modelo Payment
+        on_delete=models.CASCADE,  # Elimina el CustomProduct si se elimina el Payment
+        related_name='custom_products',  # Relación inversa desde Payment
+        null=True,  # Permite que un CustomProduct no esté asociado a un Payment inicialmente
+        blank=True
+    )
     name = models.CharField(_("Descripción"), max_length=100)
     color = models.CharField(_("Color"), max_length=100)
     talle = models.CharField(_("Talle"), max_length=100)
     precio = models.DecimalField(_("Precio"), max_digits=10, decimal_places=2, default=0)
     cantidad = models.PositiveIntegerField()
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name  
