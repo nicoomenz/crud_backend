61f9cc6bfe280f685819da89038b8a743573dac5
Nicolas Mendez <nicolas.mendez@t-mobs.com>
2025-05-21 17:15:25 -0300

    refactoring payment update, change email to null

--- princesse/apps/payment/serializers.py
+++ princesse/apps/payment/serializers.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, date
 from payment.models import *
 from rest_framework import serializers
 
@@ -58,6 +58,114 @@ class PaymentSerializer(serializers.ModelSerializer):
 
         if pick_up_date and return_date and pick_up_date > return_date:
             raise serializers.ValidationError("La fecha de retiro no puede ser posterior a la fecha de devolución.")
+
+        return data
+
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data['payment_date'] = instance.payment_date.strftime('%d/%m/%Y') if instance.payment_date else None
+        data['pick_up_date'] = instance.pick_up_date.strftime('%d/%m/%Y') if instance.pick_up_date else None
+        data['return_date'] = instance.return_date.strftime('%d/%m/%Y') if instance.return_date else None
+        productos_representation = []
+        for producto in instance.productos.all():
+            payment_product = PaymentProduct.objects.filter(payment=instance, producto=producto).first()
+            producto_data = {
+                'id': producto.id,
+                'categoria': CategoriaSerializer(producto.categoria).data,
+                'marca': MarcaSerializer(producto.marca).data if producto.marca else None,
+                'color': ColorSerializer(producto.color).data,
+                'talle': TalleSerializer(producto.talle).data,
+                'precio': PrecioProductoSerializer(producto.precio).data,
+                'cantidad': payment_product.cantidad if payment_product else None
+            }
+            productos_representation.append(producto_data)
+        data['productos'] = productos_representation
+        data['productos'].extend(list(instance.custom_products.filter(is_active=True).values('name', 'color', 'talle', 'precio', 'cantidad')))
+        combos_representation = []
+        for combo in instance.combo.all():
+            payment_combo = PaymentCombo.objects.filter(payment=instance, combo=combo).first()
+            combo_data = {
+                'id': combo.id,
+                'marca': MarcaSerializer(combo.marca).data if combo.marca else None,
+                'color': ColorSerializer(combo.color).data,
+                'talle': TalleSerializer(combo.talle).data,
+                'precio': PrecioComboSerializer(combo.precio).data,
+                'cantidad': payment_combo.cantidad if payment_combo else None
+            }
+            productos_data = []
+            for producto in combo.productos.all():  # O la relación adecuada para obtener los productos del combo
+                payment_product = PaymentProduct.objects.filter(payment=instance, producto=producto).first()
+                producto_data = {
+                    'categoria': CategoriaSerializer(producto.categoria).data,
+                    'marca': MarcaSerializer(producto.marca).data if producto.marca else None,
+                    'color': ColorSerializer(producto.color).data,
+                    'talle': TalleSerializer(producto.talle).data,
+                    'precio': PrecioProductoSerializer(producto.precio).data,
+                    'cantidad': payment_product.cantidad if payment_product else None
+                }
+                productos_data.append(producto_data)
+            
+            # Añadimos los productos al combo_data
+            combo_data['productos'] = productos_data
+            combos_representation.append(combo_data)
+        data['combo'] = combos_representation
+        # data['custom_product'] = instance.custom_products.all().values('name', 'color', 'talle', 'precio', 'cantidad')
+        return data
+
+class PaymentUpdateSerializer(serializers.ModelSerializer):
+    productos = ProductSerializer(many=True, required=False)
+    combo = ComboDetailSerializer(many=True, required=False)
+
+    class Meta:
+        model = Payment
+        fields = ['payment_id', 'payment_date', 'small_amount_ok', 'small_amount', 'subtotal_amount', 'descuento', 'detail_amount',  
+                  'total_amount', 'pick_up_date', 'return_date', 'price_type', 'description', 'status', 'productos', 'combo']
+
+    def to_internal_value(self, data):
+        # Validar y convertir las fechas antes de todo lo demás
+        for date_field in ['pick_up_date', 'return_date']:
+            if date_field in data:
+                try:
+                    # Convertir de DD/MM/YYYY a objeto `date`
+                    data[date_field] = datetime.strptime(data[date_field], "%d/%m/%Y").date()
+                except ValueError:
+                    raise serializers.ValidationError({
+                        date_field: "Formato de fecha inválido. Use DD/MM/YYYY."
+                    })
+
+        # Llamar al método original para procesar el resto de los datos
+        return super().to_internal_value(data)
+
+    def validate(self, data):
+        productos = data.get('productos', getattr(self.instance, 'productos', []))
+        combo = data.get('combo', getattr(self.instance, 'combo', []))
+
+        #Validar que haya al menos un producto o combo en creación o actualización
+        if not productos and not combo:
+            raise serializers.ValidationError("Debe haber al menos un producto o un combo.")
+
+        #Validar las fechas siempre
+        pick_up_date = data.get('pick_up_date') or getattr(self.instance, 'pick_up_date', None)
+        return_date = data.get('return_date') or getattr(self.instance, 'return_date', None)
+
+        if pick_up_date and return_date and pick_up_date > return_date:
+            raise serializers.ValidationError("La fecha de retiro no puede ser posterior a la fecha de devolución.")
+        
+        today = date.today()
+
+        # Validar contra hoy SOLO si es creación o si cambió la fecha
+        if not self.instance or (
+            'pick_up_date' in data and pick_up_date != self.instance.pick_up_date
+        ):
+            if pick_up_date and pick_up_date < today:
+                raise serializers.ValidationError("La fecha de retiro no puede ser anterior a hoy.")
+
+        if not self.instance or (
+            'return_date' in data and return_date != self.instance.return_date
+        ):
+            if return_date and return_date < today:
+                raise serializers.ValidationError("La fecha de devolución no puede ser anterior a hoy.")
 
         return data
 
--- princesse/apps/payment/views.py
+++ princesse/apps/payment/views.py
@@ -2,7 +2,8 @@ import json
 from django.forms import ValidationError
 from django.shortcuts import get_object_or_404, render
 from payment.models import *
-from payment.serializers import PaymentProductSerializer, PaymentSerializer, PaymentComboSerializer
+from payment.serializers import PaymentProductSerializer, PaymentSerializer, PaymentComboSerializer, PaymentUpdateSerializer
+from payment.services import PaymentService
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from django.db import transaction
@@ -243,244 +244,26 @@ class PaymentsViewSet(viewsets.ModelViewSet):
                 logger.error(f"Error al crear el pago: {str(e)}", exc_info=True)
                 return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
     
+
     def update(self, request, *args, **kwargs):
-        with transaction.atomic():
-            instance = self.get_object()
-            data = request.data
-            check_data = {
-                                        'payment_id': data['payment_id'],
-                                        'status': data['status'],
-            }
-            try:
-                
-                payment_serializer = PaymentSerializer(instance, data=data, partial=True)
-                if payment_serializer.is_valid():
-                    validated_data = payment_serializer.validated_data
-                    previous_status = instance.status
+        instance = self.get_object()
+        serializer = PaymentUpdateSerializer(instance, data=request.data, partial=True)
+        try:
+            serializer.is_valid(raise_exception=True)  # Validamos y convertimos aquí
+            validated_data = serializer.validated_data  # Datos limpios y con fechas en formato date
 
-                    # Si se envió el cliente, actualizamos
-                    if 'client' in validated_data:
-                        client_data = validated_data.pop('client')
-                        try:
-                            client = ClientPayer.objects.get(id=client_data['id'])
-                            for key, value in client_data.items():
-                                if getattr(client, key) != value:
-                                    setattr(client, key, value)
-                            client.save()
-                            instance.client = client
-                            logger.info(f"Cliente modificado: {client}")
-                        except ClientPayer.DoesNotExist:
-                            raise ValidationError("El cliente con el ID proporcionado no existe")
-                        
-                    # Actualizamos los productos
-                    if 'productos' in validated_data:
-                        productos_data = validated_data.pop('productos', None)
+            service = PaymentService(instance, validated_data)
+            updated_instance = service.process_update()
+            logger.info(f"se modificó el recibo {instance} correctamente")
 
-                    # Si no vienen productos, tomamos los productos asociados a la instancia
-                    
-                    elif check_data == data:
-                        productos_data = instance.productos.all()
-                    
-                    
-                    for producto_data in productos_data:
-                        
-                        if isinstance(producto_data, Producto):
-                            producto_data = {
-                                'categoria': producto_data.categoria,
-                                'marca': producto_data.marca,
-                                'color': producto_data.color,
-                                'talle': producto_data.talle,
-                                'precio': producto_data.precio,
-                            }
+            # Serializamos la instancia actualizada para la respuesta
+            response_serializer = PaymentUpdateSerializer(updated_instance)
+            return Response(response_serializer.data, status=status.HTTP_201_CREATED)
 
-                        if 'categoria' in producto_data:
-                            logger.info(f"Procesando producto: {producto_data}")
-                            categoria_data = producto_data.pop('categoria')
-                            marca_data = producto_data.pop('marca')
-                            color_data = producto_data.pop('color')
-                            talle_data = producto_data.pop('talle')
+        except Exception as e:
+            logger.error(f"Error al crear el pago: {str(e)}", exc_info=True)
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
-                            categoria = Categoria.objects.update_or_create(id=categoria_data.id, defaults={'nombre': categoria_data.nombre})[0]
-                            marca = Marca.objects.update_or_create(id=marca_data.id, defaults={'nombre': marca_data.nombre})[0] if marca_data else None
-                            color = Color.objects.update_or_create(id=color_data.id, defaults={'nombre': color_data.nombre})[0]
-                            talle = Talle.objects.update_or_create(nombre=talle_data)[0]
-
-                            producto = Producto.objects.get(categoria=categoria, marca=marca, color=color, talle=talle)
-                            if previous_status != 'DEVUELTO' and request.data['status'] == 'DEVUELTO':
-                                check_cantidad = PaymentProduct.objects.get(payment=instance, producto=producto)
-                                if check_cantidad:
-                                    # Añadir la cantidad de productos devueltos
-                                    Producto.objects.filter(id=producto.id).update(cantidad=F('cantidad') + check_cantidad.cantidad)
-                                    logger.info(f"Cantidad actualizada para producto {producto.id}: {producto.cantidad}")
-                                logger.info(f"Cantidad de producto devuelta: {producto.categoria.nombre}, nueva cantidad: {producto.cantidad}")
-                            else:
-                                if not producto_data.get('cantidad'):
-                                    producto_data['cantidad'] = PaymentProduct.objects.get(payment=instance, producto=producto).cantidad
-                                
-                                
-                                # Actualizamos o creamos el PaymentProduct
-                                payment_product = PaymentProduct.objects.filter(payment=instance.payment_id, producto=producto).first()
-                                
-                                if payment_product:
-                                    check_data = {
-                                        'payment_id': data['payment_id'],
-                                        'status': data['status'],
-                                    }
-                                    if producto_data['cantidad'] != payment_product.cantidad and data != check_data:
-                                        if producto.cantidad < producto_data['cantidad']:
-                                            logger.error(f"No hay suficiente cantidad del producto")
-                                            raise ValidationError(f"No hay suficiente cantidad del producto: {producto.categoria.nombre}. Disponible: {producto.cantidad}, Solicitado: {producto_data['cantidad']}")
-                                        PaymentProduct.objects.filter(payment=instance.payment_id, producto=producto).update(cantidad=producto_data['cantidad'])
-                                        logger.info(f"Se actualizó la cantidad de pedidos al producto")
-                                        Producto.objects.filter(id=producto.id).update(cantidad=F('cantidad') + payment_product.cantidad - producto_data['cantidad'])
-                                        logger.info(f"Cantidad actualizada para producto {producto.id}: {producto.cantidad}")
-                                    elif data == check_data:
-                                        if previous_status == 'DEVUELTO' and request.data['status'] != 'DEVUELTO':
-                                            Producto.objects.filter(id=producto.id).update(cantidad=F('cantidad') - payment_product.cantidad)
-                                            logger.info(f"Cantidad actualizada para producto {producto.id}: {producto.cantidad}")                              
-                                else:
-                                    precio_efectivo = producto_data['precio'].efectivo
-                                    precio_debito = producto_data['precio'].debito
-                                    precio_credito = producto_data['precio'].credito
-                                    PaymentProduct.objects.create(payment=instance, producto=producto, cantidad=producto_data['cantidad'],
-                                                                precio_efectivo=precio_efectivo,
-                                                                precio_debito=precio_debito,
-                                                                precio_credito=precio_credito)
-                                    
-                                    logger.info(f"Producto añadido al pago: {producto} (cantidad: {producto_data['cantidad']})")
-                                    if previous_status == 'DEVUELTO' and request.data['status'] != 'DEVUELTO':
-                                        Producto.objects.filter(id=producto.id).update(cantidad=F('cantidad') - producto_data['cantidad'])
-                                        logger.info(f"Cantidad actualizada para producto {producto.id}: {producto.cantidad}")
-
-                                instance.productos.add(producto)
-                                logger.info(f"Cantidad actualizada para producto {producto.id}: {producto.cantidad}")
-                                instance.save()
-
-                        else:
-                            # Producto simple => CustomProduct
-                            custom_product, _ = CustomProduct.objects.update_or_create(
-                                payment=instance,
-                                name=producto_data['name'],
-                                color=producto_data.get('color'),
-                                talle=producto_data.get('talle'),
-                                precio=producto_data.get('precio', 0),
-                                cantidad=producto_data.get('cantidad', 1)
-                            )
-                            instance.custom_products.add(custom_product)
-                            logger.info(f"CustomProduct modificado para Payment {instance.payment_id}")
-                    
-                    # Actualizamos los combo
-                    if 'combo' in validated_data:
-                        combos_data = validated_data.pop('combo', None)
-
-                    # Si no vienen combo, tomamos los productos asociados a la instancia
-                    elif check_data == data:
-                        combos_data = instance.combo.all()
-                    
-                    for combo in combos_data:
-                        if isinstance(combo, Combo):
-                            combo = {
-                                'id': combo.id,
-                                'marca': combo.marca,
-                                'color': combo.color,
-                                'talle': combo.talle,
-                                'precio': combo.precio,
-                                'productos': combo.productos.all(),
-                            }
-                        combo_id = combo["id"]
-                        combo_instance = Combo.objects.get(id=combo_id)
-                        instance.combo.add(combo_instance)
-                        logger.info(f"Combo añadido al pago: {combo_instance}")
-                        productos = combo.get("productos", [])
-
-                        # Restar cantidades de los productos dentro del combo
-                        for producto_data in productos:
-                            if isinstance(producto_data, Producto):
-                                producto_data = {
-                                    'categoria': {'id': producto_data.categoria.id, 'nombre': producto_data.categoria.nombre},
-                                    'marca': {'id': producto_data.marca.id, 'nombre': producto_data.marca.nombre},
-                                    'color': {'id': producto_data.color.id, 'nombre': producto_data.color.nombre},
-                                    'talle': {'nombre': producto_data.talle.nombre},
-                                    'precio': producto_data.precio,
-                                    'cantidad': producto_data.cantidad
-                                }
-                            categoria_data = producto_data.get('categoria')
-                            marca_data = producto_data.get('marca')
-                            color_data = producto_data.get('color')
-                            talle_data = producto_data.get('talle')
-                            categoria = Categoria.objects.get_or_create(id=categoria_data['id'], defaults={'nombre': categoria_data['nombre']})[0]
-                            marca = Marca.objects.get_or_create(id=marca_data['id'], defaults={'nombre': marca_data['nombre']})[0] if marca_data else None
-                            color = Color.objects.get_or_create(id=color_data['id'], defaults={'nombre': color_data['nombre']})[0]
-                            talle = Talle.objects.get_or_create(nombre=talle_data['nombre'])[0]
-                            producto = Producto.objects.get(categoria=categoria, marca=marca, color=color, talle=talle)
-                            logger.info(f"Producto encontrado: {producto}")
-
-                            # Si el estado cambió a "DEVUELTO", revertimos la cantidad
-                            if previous_status != 'DEVUELTO' and request.data['status'] == 'DEVUELTO':
-                                check_cantidad = PaymentCombo.objects.get(payment=instance, combo=combo_instance)
-                                if check_cantidad:
-                                    # Añadir la cantidad de productos devueltos
-                                    producto.cantidad += check_cantidad.cantidad
-                                    producto.save()
-                                logger.info(f"Cantidad de producto devuelta: {producto.categoria.nombre}, nueva cantidad: {producto.cantidad}")
-                            else:
-                                if not combo.get('cantidad'):
-                                    combo['cantidad'] = PaymentCombo.objects.get(payment=instance, combo=combo_instance).cantidad
-                                
-                                payment_combo = PaymentCombo.objects.filter(payment=instance.payment_id, combo=combo_instance).first()
-                                check_data = {
-                                    'payment_id': data['payment_id'],
-                                    'status': data['status'],
-                                }
-                                if payment_combo:
-                                    if combo['cantidad'] != payment_combo.cantidad and data != check_data:
-                                        
-                                        if producto.cantidad < combo['cantidad']:
-                                            logger.error(f"No hay suficiente cantidad del producto")
-                                            raise ValidationError(f"No hay suficiente cantidad del producto: {producto.categoria.nombre}. Disponible: {producto.cantidad}, Solicitado: {combo['cantidad']}")
-                                        
-                                        Producto.objects.filter(id=producto.id).update(cantidad=F('cantidad') + payment_combo.cantidad - combo['cantidad'])
-                                        logger.info(f"Cantidad actualizada para producto {producto.id}: {producto.cantidad}")
-                                    elif data == check_data:
-                                        if previous_status == 'DEVUELTO' and request.data['status'] != 'DEVUELTO':
-                                            Producto.objects.filter(id=producto.id).update(cantidad=F('cantidad') - payment_combo.cantidad)
-                                            logger.info(f"Cantidad actualizada para producto {producto.id}: {producto.cantidad}")                              
-
-                                    logger.info(f"Cantidad actualizada para producto {producto.id}: {producto.cantidad}")
-                                else:
-                                    PaymentCombo.objects.create(payment=instance, combo=producto, cantidad=combo['cantidad'],
-                                                                precio_efectivo=precio_efectivo,
-                                                                precio_debito=precio_debito,
-                                                                precio_credito=precio_credito)
-                                    logger.info(f"Combo añadido al pago: {producto}")
-                                    if previous_status == 'DEVUELTO' and request.data['status'] != 'DEVUELTO':
-                                        Producto.objects.filter(id=producto.id).update(cantidad=F('cantidad') - combo['cantidad'])
-                                        logger.info(f"Cantidad actualizada para producto {producto.id}: {producto.cantidad}")
-                                instance.combo.add(combo_instance)
-                        if not combo.get('cantidad'):
-                            combo['cantidad'] = PaymentCombo.objects.get(payment=instance, combo=combo_instance).cantidad
-                        PaymentCombo.objects.filter(payment=instance.payment_id, combo=combo_instance).update(cantidad=combo['cantidad'])
-                        logger.info(f"Se actualizó la cantidad de pedidos al producto")
-                        instance.save()
-
-                    # Actualizamos los campos simples solo si fueron enviados
-                    for attr, value in validated_data.items():
-                        setattr(instance, attr, value)
-
-                    # Guardamos el objeto Payment actualizado
-                    
-                    instance.save()
-                    logger.info(f"Recibo modificado: {instance}")
-                    serializer = self.get_serializer(instance)
-                    return Response(serializer.data, status=status.HTTP_201_CREATED)
-                else:
-                    logger.error(f"Error al validar el pago: {payment_serializer.errors}")
-                    return Response(payment_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-            except Exception as e:
-                logger.error(f"Error al crear el pago: {str(e)}", exc_info=True)
-                return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
-        
-    
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
         if instance.status != 'DEVUELTO':
@@ -549,6 +332,3 @@ class PaymentsViewSet(viewsets.ModelViewSet):
         response = HttpResponse(pdf_buffer, content_type='application/pdf')
         response['Content-Disposition'] = f'attachment; filename=recibo_{data["payment_id"]}.pdf'
         return response
-
-
-    
